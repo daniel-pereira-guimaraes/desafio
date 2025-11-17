7f82071d8047d6309ae8e315836f36122b8ad2f5
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2025-11-17 14:59:59 -0400

    Criada a classe de aplicação que implementa o caso de uso obter pedido

---
+++ src/main/java/com/danielpg/desafio/application/order/GetOrderUseCase.java
@@ -0,0 +1,17 @@
+package com.danielpg.desafio.application.order;
+
+import com.danielpg.desafio.domain.Order;
+import com.danielpg.desafio.domain.OrderRepository;
+
+public class GetOrderUseCase {
+
+    private final OrderRepository repository;
+
+    public GetOrderUseCase(OrderRepository repository) {
+        this.repository = repository;
+    }
+
+    public Order execute(Long id) {
+        return repository.getByIdOrThrow(id);
+    }
+}
